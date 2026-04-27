50b4c5d0d72f04dde0ac9b1a5e480db967f429ea
Aditya Verma <adityaverma0999@gmail.com>
2026-04-27 16:21:45 +0530

    Implement knapsack solution using dynamic programming

---
+++ lab_external/01_knapsack.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int knapsack(int W, vector<int>& val, vector<int>& wt) {
+        int n = val.size();
+        vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+        
+        for (int i = 1; i <= n; i++) {
+            for (int w = 0; w <= W; w++) {
+              
+                dp[i][w] = dp[i-1][w];
+                
+             
+                if (wt[i-1] <= w) {
+                    dp[i][w] = max(dp[i][w], dp[i-1][w - wt[i-1]] + val[i-1]);
+                }
+            }
+        }
+        
+        return dp[n][W];
+    }
+};
